cdb6c1ad5d87ff190b5a30b8b53dbe7f32c69458
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-11-21 00:23:44 -0800

    updated config file for testing

--- config.js
+++ config.js
@@ -8,14 +8,17 @@ const PORT = +process.env.PORT || 3000;
 let DB_URI;
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = "jobly-test";
+  DB_URI = "mongodb://127.0.0.1:27017";
+  DB_NAME = "earlystage-due-diligence-test";
 } else {
-  DB_URI = process.env.DATABASE_URL || "jobly";
+  DB_URI = process.env.DB_URI;
+  DB_NAME = process.env.DB_NAME;
 }
 
 module.exports = {
   SECRET_KEY,
   PORT,
   DB_URI,
+  DB_NAME,
   BCRYPT_WORK_FACTOR
 };
