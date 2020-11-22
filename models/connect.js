6640445752754658fc2cd4279bd67b4c07fb67f2
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-11-21 23:49:23 -0800

    updated DB_URI vars

--- models/connect.js
+++ models/connect.js
@@ -4,14 +4,16 @@ const { DB_URI, DB_NAME } = require('../config');
 
 let db
 
-MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-  if (err) throw new ExpressError('Problem connecting to database', 500);
+MongoClient.connect(DB_URI, { useNewUrlParser: true }, (err, client) => {
+  if (err) console.log(err);
 
   // Storing a reference to the database so you can use it later
   db = client.db(DB_NAME)
-  
-  console.log(`Connected MongoDB: ${url}`)
-  console.log(`Database: ${dbName}`)
+
+  console.log(db);
+
+  console.log(`Connected MongoDB: ${DB_URI}`)
+  console.log(`Database: ${DB_NAME}`)
 });
 
 module.exports = db;
