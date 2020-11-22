2006855bedb23bdf555cc19f0a3e261c2a334ac0
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-11-21 22:47:11 -0800

    throw error instead of reading to console

--- models/connect.js
+++ models/connect.js
@@ -5,10 +5,11 @@ const { DB_URI, DB_NAME } = require('../config');
 let db
 
 MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
-  if (err) return console.log(err)
+  if (err) throw new ExpressError('Problem connecting to database', 500);
 
   // Storing a reference to the database so you can use it later
   db = client.db(DB_NAME)
+  
   console.log(`Connected MongoDB: ${url}`)
   console.log(`Database: ${dbName}`)
 });
