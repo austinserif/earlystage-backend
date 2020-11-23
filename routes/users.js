05fe1b10e57323b8644ba62575d539365985f879
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-11-22 22:47:29 -0800

    get users and newUser routes

--- routes/users.js
+++ routes/users.js
@@ -15,31 +15,61 @@ const { authorizeCertainUser, authorizeAdmin } = require('../middleware/route-pr
  * GET /users
  * access: admin only
  */
-// router.get('/', authorizeAdmin, async function(request, response, next) {
-//     try {
-//         const { users } = await User.readAll(); //pass params, will be read by user.get() as an options obj and destructured within function.
-//         return response.json({ users });
-//     } catch(err) {
-//         return next(err);
-//     }
-// });
+router.get('/', async function(request, response, next) {
+    try {
+        const result = await User.getAllUsers(); //pass params, will be read by user.get() as an options obj and destructured within function.
+        return response.json(result);
+    } catch(err) {
+        return next(err);
+    }
+});
+
+
 
 /**
- * GET /users/:id
+ * GET /users/:email
  * access: certain user only
 */
-// router.get('/', authorizeCertainUser, async function(request, response, next) {
-//     try {
-//         //get userId from request params object
-//         const { userId } = request.params;
-//         const user = await User.getUserById(userId);
-//         return response.json({ user });
-//     } catch(err) {
-//         return next(err);
-//     }
-// });
+router.get('/:email', async function(request, response, next) {
+    try {
+        //get userEmail from request params object
+        const { email } = request.params;
+        const decodedEmail = decodeURIComponent(email);
+        const user = await User.getUserByEmail(decodedEmail);
+        return response.json(user);
+    } catch(err) {
+        return next(err);
+    }
+});
 
 /** 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * {
+    "newUser": {
+        "result": {
+            "ok": 1,
+            "n": 1
+        },
+        "ops": [
+            {
+                "name": "Austin",
+                "email": "aserif@me.com",
+                "password": {},
+                "_id": "5fbb33dcfd52a72383f3285f"
+            }
+        ],
+        "insertedCount": 1,
+        "insertedIds": {
+            "0": "5fbb33dcfd52a72383f3285f"
+        }
+    }
+}
+
+
  * POST /users
  * access: anyone
 */
@@ -48,11 +78,13 @@ router.post('/', async function(request, response, next) {
         //get user object from request.body
         const { name, email, password } = request.body;
 
+        //validate schema
+
         //create new user 
         const newUser = await User.createNewUser({ name, email, password });
 
         //return response
-        return response.json({ newUser });
+        return response.json(newUser);
     } catch(err) {
         return next(err);
     }
