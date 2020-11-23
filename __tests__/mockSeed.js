281adc8ed52b153b67536becf0a6a9c272a83209
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-11-23 13:44:34 -0800

    created mock seed file for user testing

---
+++ __tests__/mockSeed.js
@@ -0,0 +1,20 @@
+const createUserDocument = require('../helpers/createUserDocument');
+const mockUsersArray = [
+    { 
+        name: 'Testy Tester',
+        email: 'testuser@email.com',
+        password: 'superSecretPassword'
+    },
+    { 
+        name: 'Testy Tester the 2nd',
+        email: 'testuser2@email.com',
+        password: 'superSecretPassword2!'
+    }
+]
+
+/** returns an array of mock users ready to be seeded */
+const getMockUsers = () => {
+    return mockUsersArray.map(({name, email, password}) => (createUserDocument({name, email, password})));
+}
+
+module.exports = getMockUsers;
