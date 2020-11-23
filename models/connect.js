cf959014d0e21ea27188b384423fd77770ee5ac8
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-11-22 22:47:02 -0800

    went back to generating client instance from func

--- models/connect.js
+++ models/connect.js
@@ -1,22 +1,34 @@
 /** Contains logic for creating instance of mongodb connection */
 const { MongoClient } = require('mongodb');
 const { DB_URI, DB_NAME } = require('../config');
+const ExpressError = require('../helpers/ExpressError');
 
-let db
+/** returns a database */
+const getConnection = async () => {
+  
+  
+  try {
+    //create a new database cluster client instance
+    const client = new MongoClient(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    
+    //connect to database cluster
+    await client.connect();
 
-MongoClient.connect(DB_URI, { useNewUrlParser: true }, (err, client) => {
-  if (err) console.log(err);
+    //establish connection with specific database by name
+    const db = client.db(DB_NAME);
 
-  // Storing a reference to the database so you can use it later
-  db = client.db(DB_NAME)
+    return db;
 
-  console.log(db);
+  } catch (err) {
+    //read msg to console
+    console.log(err);
 
-  console.log(`Connected MongoDB: ${DB_URI}`)
-  console.log(`Database: ${DB_NAME}`)
-});
+    //throw internal error
+    throw new ExpressError('There was an error connecting with the server', 500);
+  }
+}
 
-module.exports = db;
+module.exports = getConnection;
 
 
 
