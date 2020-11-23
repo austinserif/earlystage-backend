2933c4ebb7d675473d24883450af420c1e02c598
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-11-23 13:00:07 -0800

    added helper function for fitting full user schema

--- helpers/createUserDocument.js
+++ helpers/createUserDocument.js
@@ -1,1 +1,23 @@
-/** Given a user object that includes */
+/** 
+ * Takes a new user object with basic information, and fits it into 
+ * a json schema that includes other necessary fields for later on in 
+ * the userflow
+ */
+const createUserDocument = ({ name, email, password }) => {
+    const now = new Date();
+    return {
+        email,
+        account: {
+            name,
+            password
+        },
+        workspaces: {},
+        questions: {},
+        metadata: {
+            lastModified: now,
+            createdDate: now
+        }
+    }
+}
+
+module.exports = createUserDocument;
