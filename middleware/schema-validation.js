7010b221c9b4e73527582783a1c7aafa4e1b3ac9
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-11-24 23:55:25 -0800

    added schema for updating user

--- middleware/schema-validation.js
+++ middleware/schema-validation.js
@@ -1,4 +1,5 @@
 const userSchema = require('../schema/user/newUser.json');
+const updateSchema = require('../schema/user/updateUser.json')
 const jsonschema = require('jsonschema');
 const ExpressError = require('../helpers/expressError');
 
@@ -20,4 +21,28 @@ function validateNewUser(request, response, next) {
     }
 }
 
-module.exports = { validateNewUser };
+function validateUpdatedUser(request, response, next) {
+    try {
+        const { updates } = request.body;
+
+        const parsedUpdates = JSON.parse(updates);
+
+        const result = jsonschema.validate({updates: parsedUpdates}, updateSchema);
+
+        if (result.errors.length) {
+            // pass a 400 error to the error-usernamer
+            let listOfErrors = result.errors.map(err => err.stack);
+            const err = new ExpressError(listOfErrors, 400);
+            console.log(result);
+            return next(err); 
+        }
+
+        request.body.updates = parsedUpdates;
+
+        return next();
+    } catch(err) {
+        return next(err);
+    }
+}
+
+module.exports = { validateNewUser, validateUpdatedUser };
