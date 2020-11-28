ddded335d5bf763343b0563478e1608d5cd71c23
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-11-27 23:42:56 -0800

    get, post, and patch routes add to workspace

--- routes/workspaces.js
+++ routes/workspaces.js
@@ -4,8 +4,9 @@ const express = require('express');
 const ExpressError = require('../helpers/expressError');
 const router = express.Router();
 const User = require('../models/user');
+const Workspace = require('../models/workspace');
 const jsonschema = require('jsonschema');
-const { validateNewUser, validateUpdatedUser } = require('../middleware/schema-validation');
+const { validateUpdatedWorkspace } = require('../middleware/schema-validation');
 const bcrypt = require('bcrypt');
 const { BCRYPT_WORK_FACTOR } = require('../config');
 const { authorizeCertainUser, authorizeAdmin } = require('../middleware/route-protection');
@@ -25,6 +26,23 @@ router.get('/:email', authorizeCertainUser, async function(request, response, ne
 });
 
 /** 
+ * GET /users/workspaces/:email/:workspaceId 
+ * 
+ * */
+router.get('/:email/:workspaceId', authorizeCertainUser, async function(request, response, next) {
+    try {
+        const { email, workspaceId } = request.params;
+        const res = await Workspace.getById(workspaceId, email);
+        if (!!res) {
+            return response.json(res);
+        }
+        throw new ExpressError('Unauthorized: You can only view your own workspaces', 401)
+    } catch (err) {
+        return next(err);
+    }
+});
+
+/** 
  * POST /users/workspaces/:email
  * 
  * create a new workspace for a given user
@@ -33,11 +51,34 @@ router.get('/:email', authorizeCertainUser, async function(request, response, ne
 router.post('/:email', authorizeCertainUser, async function(request, response, next) {
     try {
         const { email } = request.params;
-        const { workspace } = request.body;
-        const res = await Workspaces.new(email, workspace);
+        const { name, domain } = request.body;
+        const res = await Workspace.new(email, { companyName: name, companyDomain: domain});
+        return response.json(res);
     } catch(err) {
         return next(err);
     }
-})
+});
+
+/** 
+ * PATCH /users/workspaces/:email/:workspaceId
+ * 
+ * edit the name and domain fields of a workspace
+ */
+router.patch('/:email/:workspaceId', authorizeCertainUser, validateUpdatedWorkspace, async function(request, response, next) {
+    try {
+        const { email, workspaceId } = request.params;
+
+        const { updates } = request.body;
+
+        //decode email
+        const decodedEmail = decodeURIComponent(email);
+
+        const res = await Workspace.update(workspaceId, decodedEmail, updates);
+
+        return res;
+    } catch(err) {
+        return next(err);
+    }
+});
 
 module.exports = router;
