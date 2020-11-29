ccb69d07d251cd470f457c8cce0a1503eba4bf98
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-11-28 19:49:53 -0800

    added createWorkspaceDocument to createDoc

---
+++ helpers/createDocument.js
@@ -0,0 +1,40 @@
+/** 
+ * Takes a new user object with basic information, and fits it into 
+ * a json schema that includes other necessary fields for later on in 
+ * the userflow
+ */
+const createUserDocument = ({ name, email, password, isAdmin, verificationCode }) => {
+    const now = new Date();
+    return {
+        email,
+        account: {
+            name,
+            password,
+            isVerified: false,
+            verificationCode,
+            isAdmin,
+        },
+        workspaces: [],
+        questions: [],
+        metadata: {
+            lastModified: now,
+            createdDate: now
+        }
+    }
+}
+
+const createComponentDocument = ({questionId, workspaceId, answer}) => {
+    const now = new Date();
+    return {
+        questionId,
+        workspaceId,
+        answer,
+        readiness: 'draft',
+        metadata: {
+            lastModified: now,
+            createdDate: now
+        }
+    }
+}
+
+module.exports = { createUserDocument, createComponentDocument };
