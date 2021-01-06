bf51dcb2f2266935cd31e5679c4767c38c1ea7cc
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-01-05 23:40:40 -0800

    error

--- routes/components.js
+++ routes/components.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ExpressError = require('../helpers/expressError');
+const ExpressError = require('../helpers/ExpressError');
 
 const User = require('../models/user');
 const Component = require('../models/component');
