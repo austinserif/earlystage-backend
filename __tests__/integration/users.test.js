135b716c6d5df3d2a0e92aeeb43defdcfa71814a
Austin Serif <austin@Austins-MacBook-Pro.local>
2021-03-07 00:34:03 -0800

    added async/await statements to test

--- __tests__/integration/users.test.js
+++ __tests__/integration/users.test.js
@@ -1,114 +1,89 @@
 process.env.NODE_ENV = "test";
-const request = require("supertest");
 const app = require("../../app");
-const db = require("../../db");
-const bcrypt = require('bcrypt');
-const { SECRET_KEY, BCRYPT_WORK_FACTOR } = require('../../config');
-const jwt = require('jsonwebtken');
-let testUserToken;
-const mock = require("../mockFunctions/users");
+const supertest = require("supertest");
+const request = supertest(app);
 
-describe("GET /users/:email", function() {
-    test("", async function() {
-        const response = await request(app)
-            .post(`/users`)
-            .send()
-    });
-
-});
+const mock = require("../../mockFunctions/users");
+const getConnection = require('../../models/connect');
+const firstUser = mock.createFakeUser();
 
 describe("POST /users", function() {
-    const { name, email, password } = mock.createFakeUser();
+    // set up 
+    const { name, email, password } = firstUser;   
 
     // try creating a new user with an email that does not exist
     test("Create a new user", async function() {
-        request(app)
-            .post(`/users`)
-            .send({
-                name,
-                email,
-                password
-            })
-            .expect(201, {
-                message: "Registration successful!" 
-            })
-            .end(function(err, res) {
-              if (err) throw err;
-            });
-        }
-    );
+        const body = { name, email, password };
+        const response = await request.post(`/users`).send(body)
+        expect(response.status).toBe(201);
+        expect(response.body.message).toBe("Registration successful!")
+    });
 
     // try creating that same user again 
     test("Refuses to create a user that already exists", async function() {
-        request(app)
-            .post(`/users`)
-            .send({
-                name,
-                email,
-                password
-            })
-            .expect(409, {
-                message: "The email you chose is already in use" 
-            })
-            .end(function(err, res) {
-              if (err) throw err;
-            });
-        }
-    );
+        const body = { name, email, password };
+        const response = await request.post(`/users`).send(body)
+        expect(response.status).toBe(409);
+        expect(response.body.message).toBe("The email you chose is already in use")
+    });
 
     // try creating a user when required fields are missing
     test("Returns message array and 400 status when required fields are left blank", async function() {
-        request(app)
-            .post(`/users`)
-            .send({
-                password
-            })
-            .expect(400, {
-                message: [
-                    "instance.email does not conform to the \"email\" format",
-                    "instance.name does not meet minimum length of 2"
-                ]
-            })
-            .end(function(err, res) {
-              if (err) throw err;
-            });
-        }
-    );
-});
-
-describe("PATCH /users/:email", function() {
-
-});
-
-describe("DELETE /users/:email", function() {
-});
-
-describe("POST /users", function() {
-    test("register a new user and return a token", async function() {
-        const response = await request(app)
-            .post(`/register`)
-            .send({
-                username: "",
-                first_name: "Alyssa",
-                last_name: "Plese",
-                password: "secret",
-                email: "aplese1@yahoo.com"
-            });
-
-        expect(Object.keys(response.body)[0]).toBe("token");
-    });
-
-
-    test("throw error when user doesn'y comply with JSON schema", async function() {
-        const response = await request(app)
-            .post(`/register`)
-            .send({
-                username: "aplese",
-                first_name: "Alyssa",
-                password: "secret",
-                email: "aplese1@yahoo.com"
-            });
-
-        expect(response.body.message).toBe(`null value in column "last_name" violates not-null constraint`);
+        const response = await request.post(`/users`).send({ name: "", email: "", password });
+        expect(response.status).toBe(400);
+        expect(response.body.message.includes(`instance.email does not conform to the \"email\" format`)).toBe(true)
+        expect(response.body.message.includes("instance.name does not meet minimum length of 2")).toBe(true);
     });
 });
+
+describe("POST /login", function() {
+    test("login to get new data", async function() {
+        const { email, setToken, setUid } = firstUser; 
+        const response = await request.post(`/login`).send({ email, password: firstUser.password })
+        expect(response.status).toBe(200); // check for successful response
+        expect('token' in response.body).toBe(true); // check that token is retured
+        expect('uid' in response.body).toBe(true);
+        expect('isVerified' in response.body).toBe(true);
+        expect('email' in response.body).toBe(true);
+
+        expect(new String(response.body.email)).toStrictEqual(new String(email))
+       
+
+        // store token for other tests
+        setToken(new String(response.body.token));
+        setUid(new String(response.body.uid));
+    })
+});
+
+
+
+describe("GET /users/:email", function() {
+    
+    test("get data about newly created user", async function() {
+        const token = firstUser.getToken();
+        const response = await request.get(`/users/${firstUser.email}?_token=${token}`);
+        expect(response.status).toBe(200);
+        expect(new String(response.body._id)).toStrictEqual(new String(mock.getUids()[0]))
+    });
+});
+
+describe('DELETE /users/:email', function() {
+    test('delete a user', async function () {
+        const token = firstUser.getToken();
+        const response = await request.delete(`/users/${firstUser.email}?_token=${token}`).send({ password: firstUser.password });
+        expect(response.status).toBe(200);
+        expect(response.body.message).toStrictEqual('Resource deleted')
+    })
+})
+
+// describe("DELETE /users/:email", function() {
+// });
+
+afterAll(async () => {
+    const [ db, client ] = await getConnection();
+    try {
+        await db.collection('users').deleteMany({});
+    } finally {
+        await client.close();
+    }
+})
