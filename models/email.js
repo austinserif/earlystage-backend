9601c3acdc3a7039877abcce8c61c694b49c9141
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-02 19:37:22 -0700

    email class definition and create acct method

--- models/email.js
+++ models/email.js
@@ -1,18 +1,66 @@
-const { send } = require('../services/email');
+const emailServices = require('../services/email');
+const databaseOps = require('../services/mongodb/databaseOps');
+const ExpressError = require('../helpers/ExpressError');
 
 class Email {
     static async sendCreateAccountLink (email) {
         try {
-            // check email against database
+            // establish connection with database client
+            const usersOps = await databaseOps('users');
 
-            // if email is associated with an active account, throw an error
+            // query database for account associated with the passed email
+            // getPage returns an array, but since we are looking or a single item
+            // we assign the variable `account` to the element at 0th array index
+            const [ account ] = await usersOps.getPage({ email });
 
-            // if email is associated with an unfinished registration generate a url and send email
+            // if account is undefined, then build url and send email
+            if (account === undefined) {
+                // get send function from email services layer
+                const { send } = emailServices();
 
-            // if email is unique and valid, generate a url and send email
+                const subject = 'Fininsh Creating your Earlystaged Due Diligence Account!';
+
+                const text = 'Go to https://earlystaged.io';
+
+                // define message configuration object
+                const messageConfig = {
+                    recipient: email,
+                    subject,
+                    text
+                };
+
+                const response = await send();
+
+                return response;
+            } else if (account.account.isVerified === false) {
+                // re-send email
+                // possibly display a message?
+
+                // get send function from email services layer
+                const { send } = emailServices();
+
+                const subject = 'Fininsh Creating your Earlystaged Due Diligence Account!';
+
+                const text = 'Go to https://earlystaged.io';
+
+                // define message configuration object
+                const messageConfig = {
+                    recipient: email,
+                    subject,
+                    text
+                };
+
+                const response = await send();
+
+                return response;
+                
+            } else if (account.account.isVerified === true) {
+                // throw an error 
+                throw new ExpressError('A user is already registered with this email!');
+            }
 
         } catch (err) {
-            // handle errors
+            throw new ExpressError(err.message, err.status || 500);
         }
     };
     static async sendResetPasswordLink () {};
