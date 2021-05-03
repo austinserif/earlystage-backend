f20c59ef17c9131ced2f042438c7c4e173c359ba
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-02 19:36:07 -0700

    exporting more mailgun keys from config file

--- config.js
+++ config.js
@@ -2,17 +2,20 @@
 require("dotenv").config();
 
 const SECRET_KEY = process.env.SECRET_KEY || "test";
-const BCRYPT_WORK_FACTOR = process.env.BCRYPT_WORK_FACTOR;
-const PORT = +process.env.PORT || 3000;
+const PORT = +process.env.PORT || 4000;
+const { MAILGUN_API_KEY,MAILGUN_SENDING_KEY, MAILGUN_PUBLIC_KEY, MAILGUN_BASE_URL, BCRYPT_WORK_FACTOR } = process.env;
 
 let DB_URI;
+let SENDER_DOMAIN;
 
 if (process.env.NODE_ENV === "test") {
   DB_URI = process.env.TEST_DB_URI;
   DB_NAME = process.env.TEST_DB_NAME;
+  SENDER_DOMAIN = process.env.TEST_SENDER_DOMAIN;
 } else {
   DB_URI = process.env.DB_URI;
   DB_NAME = process.env.DB_NAME;
+  SENDER_DOMAIN = process.env.SENDER_DOMAIN;
 }
 
 module.exports = {
@@ -20,5 +23,10 @@ module.exports = {
   PORT,
   DB_URI,
   DB_NAME,
-  BCRYPT_WORK_FACTOR
+  BCRYPT_WORK_FACTOR,
+  SENDER_DOMAIN,
+  MAILGUN_API_KEY,
+  MAILGUN_PUBLIC_KEY,
+  MAILGUN_BASE_URL,
+  MAILGUN_SENDING_KEY
 };
