865d3534dd43712fbec75b9b3aeb809d9be7eb04
Austin Serif <58060032+austinserif@users.noreply.github.com>
2021-05-10 20:20:05 -0700

    moved email checking logic outof sendCreateActLink

--- models/email.js
+++ models/email.js
@@ -1,67 +1,31 @@
 const emailServices = require('../services/email');
-const databaseOps = require('../services/mongodb/databaseOps');
 const ExpressError = require('../helpers/ExpressError');
 
 class Email {
-    static async sendCreateAccountLink (email) {
+    static async sendCreateAccountLink ({ email, _id }) {
         try {
-            // establish connection with database client
-            const usersOps = await databaseOps('users');
 
-            // query database for account associated with the passed email
-            // getPage returns an array, but since we are looking or a single item
-            // we assign the variable `account` to the element at 0th array index
-            const [ account ] = await usersOps.getPage({ email });
+            // get send method from email services closure
+            const { send } = emailServices();
 
-            // if account is undefined, then build url and send email
-            if (account === undefined) {
-                // get send function from email services layer
-                const { send } = emailServices();
+            // create URL
+            const accountVerificationLink = `https://earlystaged.io/complete-sign-up?code=${_id}`
 
-                const subject = 'Fininsh Creating your Earlystaged Due Diligence Account!';
+            // define message configuration object
+            const messageConfig = {
+                recipient: email,
+                subject: 'Finish Creating your Earlystaged account!',
+                text: `Click this link to finish creating your account: ${accountVerificationLink}`
+            };
 
-                const text = 'Go to https://earlystaged.io';
+            const response = await send(messageConfig);
 
-                // define message configuration object
-                const messageConfig = {
-                    recipient: email,
-                    subject,
-                    text
-                };
-
-                const response = await send();
-
-                return response;
-            } else if (account.account.isVerified === false) {
-                // re-send email
-                // possibly display a message?
-
-                // get send function from email services layer
-                const { send } = emailServices();
-
-                const subject = 'Fininsh Creating your Earlystaged Due Diligence Account!';
-
-                const text = 'Go to https://earlystaged.io';
-
-                // define message configuration object
-                const messageConfig = {
-                    recipient: email,
-                    subject,
-                    text
-                };
-
-                const response = await send();
-
-                return response;
-                
-            } else if (account.account.isVerified === true) {
-                // throw an error 
-                throw new ExpressError('A user is already registered with this email!');
-            }
-
+            return response;
         } catch (err) {
             throw new ExpressError(err.message, err.status || 500);
         }
     };
     static async sendResetPasswordLink () {};
 }
+
+module.exports = Email;
